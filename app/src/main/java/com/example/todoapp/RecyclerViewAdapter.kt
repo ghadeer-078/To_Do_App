15fa46766e7e2e5c5d8a684f13dcf543150b4ada
Recycler View App <ghadeeralshehri@outlook.com>
2021-12-01 01:47:45 +0300

    To Do App Create an application that allows users to add items to a to-do list The app should use one activity containing a Recycler View and a Floating Action Button
    The following part will be difficult, try your best and we will discuss it in class:
    Each Recycler View item should contain a Text View and a Check Box
    If the Check Box is checked, the text should be a light color, otherwise, it should be black
    
    Use a ToDo class to keep track of to-do items
    The Floating Action Button should trigger an Alert Dialog that contains an Edit Text, a cancel button, and an add button
    If the user enters a to-do item and taps the add button, a new instance of the class should be created and added to the Recycler View
    Include a menu button that allows the user to delete all checked items
    You will most likely run into an issue when deleting items, try to figure this out before using the solution
    Add a Toast message to tell the user how many items have been deleted.

---
+++ app/src/main/java/com/example/todoapp/RecyclerViewAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.todoapp
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.databinding.ItemRowBinding
+
+class ToDoItem (val content: String, var isSelected: Boolean = false)
+
+
+class RecyclerViewAdapter(private val items: ArrayList<ToDoItem>): RecyclerView.Adapter<RecyclerViewAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(val binding: ItemRowBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            ItemRowBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = items[position].content
+
+        holder.binding.apply {
+            cbItem.text = item
+            cbItem.setOnCheckedChangeListener { _, _ ->
+                if (cbItem.isChecked) {
+                    cbItem.setTextColor(Color.GRAY)
+                    items[position].isSelected = true
+                }
+                else{
+                    cbItem.setTextColor(Color.BLACK)
+                    items[position].isSelected = false
+                }
+            }
+        }
+    }
+
+    override fun getItemCount() = items.size
+
+}
